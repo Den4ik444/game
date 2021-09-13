517d6b7e5b9ad15f8eef5ace9ede9aa1a3fa973e
pupil <pupil@ivkhk.ee>
2021-09-13 13:34:52 +0300

    1

---
+++ src/game/Game.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Melnikov
+ */
+public class Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int attempt = 4;
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int number = random.nextInt(10);
+        System.out.print("Программа задумала число от 0 до 9, отгадай какое?: ");
+        for(int i = 0; i < 5; i++){
+            int userNumber = scanner.nextInt();
+            if(userNumber == number){
+                System.out.println("Ура! Ты выиграл!!!");
+                break;
+            }else{
+                if(i < attempt){
+                    System.out.println("Неправильно, попробуй еще: ");
+                }else{
+                    System.out.println(":(, проиграл, задумано: "+number);
+                }
+            }
+        }
+    }
+    
+}
